5c91301c45a98b339e100912d3c4b4530b13102e
ashish <ashishsamuelthomson@gmail.com>
2023-11-06 14:10:58 +0530

    alignment issues

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ function App() {
     <QuizContext.Provider value={{gameState,setGameState,name,setName,theme,result,setResult}}>
       
       <div className='d-flex flex-column justify-content-center align-items-center w-auto' id={theme} style={{height:'100vh'}}>
-      <div className='shadow p-5 border-rad box-width bg-color padding'>
+      <div className='shadow px-5 py-2 border-rad box-width bg-color padding'>
         {gameState === 'start' &&
         <h3 className='text-center my-5 fw-bold'>Quiz App</h3>
         }
         {gameState === 'quiz' &&
-        <h3 className='text-center mb-5'>Questions</h3>
+        <h3 className='text-center mb-4'>Questions</h3>
         }          
               {gameState==='start' && <StartGameScreen/>}
               {gameState==='quiz' && <QuizScreen/>}
